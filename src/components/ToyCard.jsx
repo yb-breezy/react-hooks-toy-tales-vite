d318e06d6dfd526cb57be607e6fe3affc7c5ef32
yb-breezy <4bd1f5299@gmail.com>
2026-05-11 12:42:52 +0300

    Fix like button text

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -22,7 +22,7 @@ function ToyCard({ toy, onDeleteToy, onLikeToy }) {
       <h2>{toy.name}</h2>
       <img src={toy.image} alt={toy.name} className="toy-avatar" />
       <p>{toy.likes} Likes</p>
-      <button className="like-btn" onClick={handleLike}>Like ❤️</button>
+    <button className="like-btn" onClick={handleLike}>Like {"<3"}</button>
       <button className="del-btn" onClick={handleDelete}>Donate to GoodWill</button>
     </div>
   );
