86c44e59e7ac2ca12991a79f8176eb85a67f5c7a
yb-breezy <4bd1f5299@gmail.com>
2026-05-11 12:48:22 +0300

    Fix likes spacing

--- src/components/ToyCard.jsx
+++ src/components/ToyCard.jsx
@@ -21,7 +21,7 @@ function ToyCard({ toy, onDeleteToy, onLikeToy }) {
     <div className="card" data-testid="toy-card">
       <h2>{toy.name}</h2>
       <img src={toy.image} alt={toy.name} className="toy-avatar" />
-      <p>{toy.likes} Likes</p>
+      <p>{toy.likes + " Likes"}</p>
     <button className="like-btn" onClick={handleLike}>Like {"<3"}</button>
       <button className="del-btn" onClick={handleDelete}>Donate to GoodWill</button>
     </div>
